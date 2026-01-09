8e9c98ea13f7f90649c97c298dfe4e23983392cf
saniyafarzindeveloper <developerfarzin@gmail.com>
2026-01-09 12:47:38 +0530

    coins table - initiated

---
+++ app/coins/page.tsx
@@ -0,0 +1,87 @@
+import { fetcher } from "@/lib/coingecko.actions";
+import DataTable from "@/components/DataTable";
+import Image from "next/image";
+import Link from "next/link";
+
+import { cn, formatPercentage, formatCurrency } from "../../lib/utils";
+
+const Coins = async ({ searchParams }: NextPageProps) => {
+
+  const coinsData = await fetcher<CoinMarketData[]>("/coins/markets", {
+    vs_currency: "usd",
+    order: "market_cap_desc",
+    sparkline: "false",
+    price_change_percentage: "24h",
+  });
+
+  const columns: DataTableColumn<CoinMarketData>[] = [
+    {
+      header: "Rank",
+      cellClassName: "rank-cell",
+      cell: (coin) => (
+        <>
+          #{coin.market_cap_rank}
+          <Link href={`/coins/${coin.id}`} aria-label="View coin" />
+        </>
+      ),
+    },
+    {
+      header: "Token",
+      cellClassName: "token-cell",
+      cell: (coin) => (
+        <div className="token-info">
+          <Image src={coin.image} alt={coin.name} width={36} height={36} />
+          <p>
+            {coin.name} ({coin.symbol.toUpperCase()})
+          </p>
+        </div>
+      ),
+    },
+    {
+      header: "Price",
+      cellClassName: "price-cell",
+      cell: (coin) => formatCurrency(coin.current_price),
+    },
+    {
+      header: "24h Change",
+      cellClassName: "change-cell",
+      cell: (coin) => {
+        const isTrendingUp = coin.price_change_percentage_24h > 0;
+
+        return (
+          <span
+            className={cn("change-value", {
+              "text-green-600": isTrendingUp,
+              "text-red-500": !isTrendingUp,
+            })}
+          >
+            {isTrendingUp && "+"}
+            {formatPercentage(coin.price_change_percentage_24h)}
+          </span>
+        );
+      },
+    },
+    {
+      header: "Market Cap",
+      cellClassName: "market-cap-cell",
+      cell: (coin) => formatCurrency(coin.market_cap),
+    },
+  ];
+
+  return (
+    <main id="coins-page">
+      <div className="content">
+        <h4>All Coins</h4>
+
+        <DataTable
+          tableClassName="coins-table"
+          columns={columns}
+          data={coinsData}
+          rowKey={(coin) => coin.id}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Coins;
