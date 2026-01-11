dff4198fe85545047e379c3cefc8a715aff4ad8a
saniyafarzindeveloper <developerfarzin@gmail.com>
2026-01-11 12:41:12 +0530

    adding custom hooks, coingecko API's not available in demo version for single coins

---
+++ hooks/useCoinGeckoWebsocket.ts
@@ -0,0 +1,3 @@
+//this file is for establishing socket connection, responsible for opening the socket, handling pings, translating incoming messages
+
+//any file that deals with live data will be using this file
